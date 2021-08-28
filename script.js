7b60744e025c6b7eac2ad9fff37e93cac5dacf47
Alvaro Vargas Alvarez <vargasalvaro248@gmail.com>
2021-08-28 12:02:23 -0400

    localStorage criado

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ var row = '<div class="subCon2" id="{{id}}">';
     row+= '<p class="precoAlto">De R$ {{precooriginal}}</p>';
     row+= '<p>Por apenas: </p>';
     row+= '<p class="preco">R$ {{precodesconto}}</p>';
+    row+= '<button type="button" class="btn btn-danger btnComprar"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg></button>';
     row+= '</button>';
     row+= '</div>';
-    row+= '<button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg></button>';
-
+    function adicionarAoCarrinho(evento){
+        let id = evento.target.parentNode.id;
+        
+        let carrinho = localStorage.getItem("compras");
+    
+        let vetorCarrinho;
+    
+        if(carrinho == null){
+            vetorCarrinho = [id];
+        }else{
+            vetorCarrinho = JSON.parse( localStorage.getItem("compras") );
+            vetorCarrinho.push(id);
+        }
+    
+        localStorage.setItem("compras", JSON.stringify(vetorCarrinho) );
+    }
+    function removercompras(){
+        localStorage.removeItem("compras");
+    }
 $(document).ready(function(){
     
     var rowProdutos = $("#container");
@@ -24,6 +42,6 @@ $(document).ready(function(){
         rowProdutos.append(htmlProduto);
     }
 
-    //rowProdutos.on("click",".btnComprar",adicionarAoCarrinho);
-
+    rowProdutos.on("click",".btnComprar",adicionarAoCarrinho);
+    var esvaziar = $("#esvaziar").on("click", removercompras);
 });
